35c3d68ef5fae11c816738a7a072147989c5aa5f
almahbuby <almahbubyanwar@protonmail.com>
2025-10-21 14:31:06 +0700

    progress

--- app/src/main/java/com/example/almahbubymidterms/MainActivity.kt
+++ app/src/main/java/com/example/almahbubymidterms/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
@@ -36,11 +37,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.almahbubymidterms.ui.theme.AlmahbubymidtermsTheme
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,9 +63,22 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+sealed class Screen(val route: String) {
+    data object Dash: Screen("Dash")
+    data object Input: Screen("Input")
+}
+
 @Composable
 fun Main(modifier: Modifier) {
-    ListContact(modifier)
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Dash.route) {
+        composable(route = Screen.Dash.route) {
+            ListContact(modifier, navController = navController)
+        }
+        composable(route = Screen.Input.route) {
+            InputContacts(modifier, onNavigateBack = {navController.popBackStack()})
+        }
+    }
 }
 
 @Composable
@@ -71,6 +91,72 @@ fun ContactItem(name: String, address: String) {
     }
 }
 
+enum class InputMode {
+    ADD,
+    EDIT
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputContacts(modifier: Modifier = Modifier, onNavigateBack: () -> Unit,
+mode: InputMode = InputMode.ADD, name: String = "", address: String = "",
+                  phone: String = "", email: String = "", index: Int = 0) {
+    var inputName by rememberSaveable { mutableStateOf(name) }
+    var inputAddress by rememberSaveable { mutableStateOf(address) }
+    var inputPhone by rememberSaveable { mutableStateOf(phone) }
+    var inputEmail by rememberSaveable { mutableStateOf(email) }
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopAppBar(colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            titleContentColor = MaterialTheme.colorScheme.primary,
+        ), title={
+            Text("Input")
+        }, navigationIcon = {
+            IconButton(onClick = onNavigateBack) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Go Back"
+                )
+            }
+        })
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(12.dp)
+            ,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            OutlinedTextField(
+                value = inputName,
+                onValueChange = {inputName = it},
+                label = {Text("Name")},
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = inputAddress,
+                onValueChange = {inputAddress = it},
+                label = {Text("Address")},
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = inputEmail,
+                onValueChange = {inputEmail = it},
+                label = {Text("Email")},
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = inputPhone,
+                onValueChange = {inputPhone = it},
+                label = {Text("Phone No.")},
+                modifier = Modifier.fillMaxWidth()
+            )
+            Button(onClick = {}) {
+                Text("Add")
+            }
+        }
+    }
+}
+
 class Contact(
     val name: String = "",
     val address: String = "",
@@ -80,7 +166,7 @@ class Contact(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ListContact(modifier: Modifier) {
+fun ListContact(modifier: Modifier, navController: NavHostController) {
     val contacts = listOf(
         Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
             phone = "+18006741", email = "bub@almahbuby.com"
@@ -90,52 +176,12 @@ fun ListContact(modifier: Modifier) {
         ),
         Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
             phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
-        Contact(name = "Bubsie", address = "Eastern St. 1, Nowhere, CA",
-            phone = "+18006741", email = "bub@almahbuby.com"
-        ),
+        )
     )
 
-    Column() {
+    val contactsState = rememberSaveable {mutableStateOf<List<Contact>>(listOf())}
+
+    Column(modifier = Modifier.fillMaxSize()) {
         TopAppBar(
             colors = TopAppBarDefaults.topAppBarColors(
                 containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -144,14 +190,14 @@ fun ListContact(modifier: Modifier) {
             title = {
             Text("Dashboard")
         })
-        Box() {
+        Box(modifier = Modifier.fillMaxSize()) {
             LazyColumn() {
                 items(contacts) {contact ->
                     ContactItem(contact.name, contact.address)
                 }
             }
             FloatingActionButton(
-                onClick = {},
+                onClick = {navController.navigate(Screen.Input.route)},
                 modifier = Modifier
                     .align(Alignment.BottomEnd)
                     .offset(x = (-32).dp, y = (-32).dp)
